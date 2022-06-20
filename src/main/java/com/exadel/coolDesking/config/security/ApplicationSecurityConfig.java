49e50a9ac6372d0209c020892d7a4c48f0b92c48
tokhirjon <tokhirjon007@gmail.com>
2022-06-20 12:23:46 +0500

    basic auth was implemented into workplace table in order to do modification.

---
+++ src/main/java/com/exadel/coolDesking/config/security/ApplicationSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.exadel.coolDesking.config.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/","index","/css/*","/js/*").permitAll()
+                .antMatchers(HttpMethod.DELETE,"/office/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/office/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT,"/office/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/office/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaUser = User.builder()
+                .username("anna")
+                .password(passwordEncoder.encode("anna"))
+                .roles("USER")
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("linda"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaUser,
+                lindaUser
+        );
+    }
+
+}
