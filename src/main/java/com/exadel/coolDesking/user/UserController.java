2c5cdae52996a8db9481712bb367c7fda55594a5
tokhirjon <tokhirjon007@gmail.com>
2022-07-08 12:22:10 +0500

    assertionError, expected 200 but provided 403

--- src/main/java/com/exadel/coolDesking/user/UserController.java
+++ src/main/java/com/exadel/coolDesking/user/UserController.java
@@ -2,6 +2,7 @@ package com.exadel.coolDesking.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,6 +15,7 @@ public class UserController {
 
     private final UserService userService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping(path = "/{id}")
     public ResponseEntity<?> getById(@PathVariable UUID id) {
         User user = userService.getUser(id);
--- src/test/java/com/exadel/coolDesking/user/UserIntegrationTest.java
+++ src/test/java/com/exadel/coolDesking/user/UserIntegrationTest.java
@@ -4,8 +4,8 @@ import com.exadel.coolDesking.common.exception.NotFoundException;
 import com.exadel.coolDesking.workspace.Workplace;
 import com.exadel.coolDesking.workspace.WorkplaceRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,28 +13,36 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 @AutoConfigureMockMvc
-@TestPropertySource("/application.properties")
 @Transactional
 @ExtendWith(SpringExtension.class)
-public class UserIntegrationTestWithMockMvc {
+public class UserIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
@@ -42,12 +50,38 @@ public class UserIntegrationTestWithMockMvc {
     private WorkplaceRepository workplaceRepository;
     @Autowired
     private ObjectMapper objectMapper;
-
     @Autowired
     private MockMvc mockMvc;
 
+    @Container
+    static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:12.3")
+            .withDatabaseName("test")
+            .withUsername("duke")
+            .withPassword("s3cret");
+
+    @Container
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"));
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", database::getJdbcUrl);
+        registry.add("spring.datasource.password", database::getPassword);
+        registry.add("spring.datasource.username", database::getUsername);
+        registry.add("spring.kafka.bootstrap-servers",kafka::getBootstrapServers);
+        registry.add("application.jwt.secretKey",()->"secretKey");
+        registry.add("application.jwt.tokenPrefix",()->"tokenPrefix");
+        registry.add("application.jwt.tokenExpirationAfterDays",()->"14");
+    }
+
+    @Sql(scripts = {"classpath:/sql/insert-office.sql", "classpath:/sql/insert-sample-floor-plan.sql", "classpath:/sql/insert-workplace.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//    @Sql(scripts = {"classpath:/sql/delete-workplace.sql", "classpath:/sql/delete-floor-plan.sql", "classpath:/sql/delete-office.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @Test
+    @DisplayName("test on add one workplace")
     void postUser() throws Exception {
+
+        Workplace workplace = workplaceRepository.findByWorkplaceNumber(1)
+                .orElseThrow(() -> new NotFoundException("There is no workplace", Workplace.class, "workplaceNumber"));
+
         UserDto userDto = new UserDto();
         userDto.setFirstName("Usmon");
         userDto.setUsername("usmon");
@@ -55,7 +89,7 @@ public class UserIntegrationTestWithMockMvc {
         userDto.setLastName("Usmon");
         userDto.setEmail("usmon@gmail.com");
         userDto.setTelegramId("12345");
-        userDto.setPreferredWorkplace(UUID.fromString("be35aac2-19a8-4cbe-9d90-21ee6ec99406"));
+        userDto.setPreferredWorkplace(workplace.getId());
         userDto.setRole("ADMIN");
 
         String jsonUser = objectMapper.writeValueAsString(userDto);
@@ -66,21 +100,19 @@ public class UserIntegrationTestWithMockMvc {
                         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2toaXIiLCJhdXRob3JpdGllcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9BRE1JTiJ9XSwiaWF0IjoxNjU3MDE0MzQ3LCJleHAiOjE3NTgxODAwMDB9.4B-7iVYsZzCpshSoMGJFg9Zjao26Lqjz8hJVKaCueU8")
                 .contentType(MediaType.APPLICATION_JSON);
 
-        mockMvc.perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(requestBuilder);
 
         Optional<User> optionalUser = userRepository.findByUsername(userDto.getUsername());
         Assertions.assertEquals(optionalUser.get().getUsername(), userDto.getUsername());
-
     }
 
+    @Sql(scripts = {"classpath:/sql/insert-office.sql", "classpath:/sql/insert-sample-floor-plan.sql", "classpath:/sql/insert-workplace.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Test
     void getUserById() throws Exception {
 
-        Optional<Workplace> byId = workplaceRepository.findById(UUID.fromString("be35aac2-19a8-4cbe-9d90-21ee6ec99406"));
+        Optional<Workplace> byId = workplaceRepository.findById(UUID.fromString("11111111-0000-0000-0000-111111111111"));
 
         User user = new User();
-        user.setId(UUID.fromString("96a74cea-a477-48f0-a703-665e4af61bf1"));
         user.setFirstName("Usmon");
         user.setUsername("usmon");
         user.setPassword("usmonjonbro");
@@ -105,3 +137,4 @@ public class UserIntegrationTestWithMockMvc {
                 .andExpect(jsonPath("$.id", is(save.getId().toString())));
     }
 }
+
